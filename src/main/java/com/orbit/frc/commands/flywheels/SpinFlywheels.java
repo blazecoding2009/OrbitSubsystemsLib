b74c55c0d8000530ae29baf2eb2f871c6ef536b1
PerfectlyInternal <alex.n.martynov@gmail.com>
2024-03-04 14:58:42 -0500

    Check to match motor, pid and speed counts

--- src/main/java/com/orbit/frc/commands/flywheels/SpinFlywheels.java
+++ src/main/java/com/orbit/frc/commands/flywheels/SpinFlywheels.java
@@ -1,5 +1,7 @@
 package com.orbit.frc.commands.flywheels;
 
+import java.lang.IllegalArgumentException;
+
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -9,6 +11,9 @@ import com.orbit.frc.subsystems.pivot.Pivot;
 import com.orbit.frc.util.OrbitTimer;
 import com.orbit.frc.util.OrbitPID;
 
+/* Command that spins a set of flywheels at certain speeds using PID controllers
+ * TODO: Maybe mod this to use intergrated PID on the SparkX controllers later
+ */
 public class SpinFlywheels extends Command {
 
     private SpinFlywheelsConfig config;
@@ -16,7 +21,9 @@ public class SpinFlywheels extends Command {
     // this changes often between commands for the same flywheels, let it be separate
     private double[] targets;
     
-    public PivotMove(PivotMoveConfig config, double... speeds) {
+    public SpinFlywheels(SpinFlywheelsConfig config, double... speeds) {
+        if(!(config.flywheels.getMotorCount() == config.pid.length && config.pid.length == speeds.length)) 
+            throw IllegalArgumentException("The number of flywheels, PID controllers and speeds must be identical");
         this.config = config;
         this.targets = speeds;
         addRequirements(config.flywheels);
