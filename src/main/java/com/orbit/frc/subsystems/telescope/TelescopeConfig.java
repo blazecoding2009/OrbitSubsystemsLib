257c36974f004a36cd9de7523e8e25885cb83b7f
PerfectlyInternal <alex.n.martynov@gmail.com>
2024-03-05 09:23:38 -0500

    fix package

---
+++ src/main/java/com/orbit/frc/subsystems/telescope/TelescopeConfig.java
@@ -0,0 +1,14 @@
+package com.orbit.frc.subsystems.telescope;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+import com.orbit.frc.subsystems.SubsystemConfig;
+
+public class TelescopeConfig extends SubsystemConfig{
+
+    /* Limit switch config
+     */
+    public int limitSwitchPort;
+    public boolean limitSwitchInverted;
+    public DigitalInput limitSwitch
+}
